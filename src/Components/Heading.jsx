31b06be47c89e2ae268e5b8ee43f5fa4a1d0d9e3
Mohammad Maheer <maheer.techxcape@gmail.com>
2021-01-01 17:48:41 +0500

    Started Using Components

---
+++ src/Components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+let name="Shoaib Arif";
+function Heading(){
+    return (
+        <div className="heading_name">
+            <h1>{name}</h1>
+        </div>
+    )
+}
+export default Heading;
---
+++ src/Components/List.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function List(){
+    return(
+        <>
+        <div className="heading_name">
+            <ol>
+                <li>Shoaib</li>
+                <li>Arif</li>
+                <li>Khatri</li>
+                <li>No</li>
+            </ol>
+        </div>
+       
+        </>
+    )
+}
+export default List;
